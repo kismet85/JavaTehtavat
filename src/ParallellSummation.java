c3b1c6632fe6f718202e0f550fb03caf875a02c2
Ismet Ymeri <kismet1985@hotmail.fi>
2024-02-14 21:59:08 +0200

    Create ParallellSummation.java

---
+++ src/ParallellSummation.java
@@ -0,0 +1,31 @@
+public class ParallelSummation {
+    public static void main(String[] args) throws InterruptedException {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int numThreads = 4; // Number of threads to use
+        int allNumbers = numbers.length / numThreads;
+
+        // Create and start threads
+        SumCalculator[] calculators = new SumCalculator[numThreads];
+        Thread[] threads = new Thread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            int start = i * allNumbers;
+            int end = (i == numThreads - 1) ? numbers.length - 1 : (i + 1) * allNumbers - 1;
+            calculators[i] = new SumCalculator(numbers, start, end);
+            threads[i] = new Thread(calculators[i]);
+            threads[i].start();
+        }
+
+        // Wait for all threads to finish
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        // Calculate total sum
+        long totalSum = 0;
+        for (SumCalculator calculator : calculators) {
+            totalSum += calculator.getSum();
+        }
+
+        System.out.println("Total sum: " + totalSum);
+    }
+}
